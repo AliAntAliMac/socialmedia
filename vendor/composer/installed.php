7e5803d29de5963f971722bf525a2aabff47130d
AAAM <aaam@gmail.com>
2025-09-24 15:35:54 +0200

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'socialmedia/socialmedia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '07c039894e5a4723075ac03d766ed10b1ef00c59',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'socialmedia/socialmedia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '07c039894e5a4723075ac03d766ed10b1ef00c59',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
